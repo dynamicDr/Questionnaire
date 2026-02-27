eafd35b902c77220adb430c7d287b862e55fe5d0
dty <1102310490@qq.com>
2026-02-28 04:38:16 +0800

    1

--- Questionnaire/settings.py
+++ Questionnaire/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&^b1u%_2lgmv-@yns_*%p6teh)wlm@b@sg@dhm&ggj0a06of%)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
